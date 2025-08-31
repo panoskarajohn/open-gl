41f9baf7142a18e2373edb75fb164fc8b56534c4
panagiotiskaragiannis <panoskarajohb@hotmail.com>
2025-08-31 21:36:06 +0100

    Do Phong shading in view space instead of world space

--- Shaders/color_1_frag.glsl
+++ Shaders/color_1_frag.glsl
@@ -3,29 +3,28 @@ out vec4 FragColor;
 
 uniform vec3 objectColor;
 uniform vec3 lightColor;
-uniform vec3 lightPos;
 uniform vec3 viewPos;
 
 
 in vec3 Normal;
 in vec3 FragPos;
+in vec3 LightPos;
 
 void main() {
-    float specularStrength = 0.1;
-
-    vec3 norm = normalize(Normal);
-    vec3 lightDir = normalize(lightPos - FragPos);
-    float diffuseStrength = 1.0;
-
-    float diff = max(dot(norm, lightDir), 0.0);
-    vec3 diffuse = diffuseStrength * diff * lightColor;
-
     float ambientStrength = 0.1;
     vec3 ambient = ambientStrength * lightColor;
 
-    vec3 viewDir = normalize(viewPos - FragPos);
+    // diffuse
+    vec3 norm = normalize(Normal);
+    vec3 lightDir = normalize(LightPos - FragPos);
+    float diff = max(dot(norm, lightDir), 0.0);
+    vec3 diffuse = diff * lightColor;
+
+    // specular
+    float specularStrength = 0.5;
+    vec3 viewDir = normalize(-FragPos);// the viewer is always at (0,0,0) in view-space, so viewDir is (0,0,0) - Position => -Position
     vec3 reflectDir = reflect(-lightDir, norm);
-    float spec = pow(max(dot(viewDir, reflectDir), 0.0), 16);
+    float spec = pow(max(dot(viewDir, reflectDir), 0.0), 32);
     vec3 specular = specularStrength * spec * lightColor;
 
     vec3 result = (ambient + diffuse + specular) * objectColor;
