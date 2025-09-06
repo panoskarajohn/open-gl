6d91a652967a73dd75ecdfa193dc62c5db8116b5
panagiotiskaragiannis <panoskarajohb@hotmail.com>
2025-09-06 12:58:07 +0100

    Add basic vertex and fragment shaders for diffuse lighting

--- main.cpp
+++ main.cpp
@@ -14,9 +14,6 @@
 #include "Utilities/VertexData.h"
 #include "VertexUtility.h"
 
-float mixValue = 0.2f;
-
-float lastFrame = 0.0f;
 float deltaTime = 0.0f; // time between current frame and last frame
 float lastX = 400, lastY = 300;
 glm::vec3 lightPos(1.2f, 1.0f, 2.0f);
@@ -26,111 +23,20 @@ Camera camera(glm::vec3(0.0f, 0.0f, 3.0f));
 const unsigned int SCR_WIDTH = 800;
 const unsigned int SCR_HEIGHT = 600;
 
+void processInput(GLFWwindow *window);
 
-void processInput(GLFWwindow *window) {
-    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) {
-        glfwSetWindowShouldClose(window, GLFW_TRUE);
-    }
+void framebuffer_size_callback(GLFWwindow *window, int width, int height);
 
-    const float cameraSensitivity = 0.05f;
-    if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS) {
-        camera.ProcessKeyboard(FORWARD, deltaTime);
-    }
-    if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS) {
-        camera.ProcessKeyboard(BACKWARD, deltaTime);
-    }
+void mouse_callback(GLFWwindow *window, double xpos, double ypos);
 
-    if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS) {
-        camera.ProcessKeyboard(LEFT, deltaTime);
-    }
-    if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS) {
-        camera.ProcessKeyboard(RIGHT, deltaTime);
-    }
-    if (glfwGetKey(window, GLFW_KEY_UP) == GLFW_PRESS) {
-        mixValue += 0.01f;
-        if (mixValue > 1.0f) {
-            mixValue = 1.0f;
-        }
-    }
+void scroll_callback(GLFWwindow *window, double xoffset, double yoffset);
 
-    if (glfwGetKey(window, GLFW_KEY_DOWN) == GLFW_PRESS) {
-        mixValue -= 0.01f;
-        if (mixValue < 0.0f) {
-            mixValue = 0.0f;
-        }
-    }
-}
-
-void mouse_callback(GLFWwindow *window, double xpos, double ypos) {
-    float xoffset = xpos - lastX;
-    float yoffset = lastY - ypos;
-    lastX = xpos;
-    lastY = ypos;
-    camera.ProcessMouseMovement(xoffset, yoffset);
-}
-
-void scroll_callback(GLFWwindow *window, double xoffset, double yoffset) {
-    camera.ProcessMouseScroll(yoffset);
-}
-
-unsigned int loadTexture(char const *path, bool invert = false) {
-    unsigned int texture;
-    glGenTextures(1, &texture);
-    glBindTexture(GL_TEXTURE_2D, texture);
-    // set the texture wrapping/filtering options (on the currently bound texture object)
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT); // horizontal
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT); // vertical
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR_MIPMAP_NEAREST); // we cannot mip map on mag
-    // load and generate the texture
-    int width, height, nrChannels;
-    if (invert == false) {
-        stbi_set_flip_vertically_on_load(true);
-    }
-    unsigned char *data = stbi_load(path, &width, &height, &nrChannels, 0);
-    if (data) {
-        GLenum format = (nrChannels == 4) ? GL_RGBA : GL_RGB;
-        glTexImage2D(GL_TEXTURE_2D, 0, format, width, height, 0, format, GL_UNSIGNED_BYTE, data);
-        glGenerateMipmap(GL_TEXTURE_2D);
-    } else {
-        std::cout << "Failed to load texture" << std::endl;
-    }
-    stbi_image_free(data);
-    return texture;
-}
+unsigned int loadTexture(char const *path, bool invert);
 
 void render_loop(GLFWwindow *window) {
     Shader lightingShader("../Shaders/color_1_vec.glsl", "../Shaders/color_1_frag.glsl");
     Shader lightCubeShader("../Shaders/color_1_vec.glsl", "../Shaders/color_1_frag_light.glsl");
 
-    glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
-    glfwSetCursorPosCallback(window, mouse_callback);
-    glfwSetScrollCallback(window, scroll_callback);
-
-    unsigned int VBO, cubeVAO;
-    glGenVertexArrays(1, &cubeVAO);
-    glGenBuffers(1, &VBO);
-
-    glBindBuffer(GL_ARRAY_BUFFER, VBO);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(cube_light), cube_light, GL_STATIC_DRAW);
-
-    glBindVertexArray(cubeVAO);
-
-    // position attribute
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void *) 0);
-    glEnableVertexAttribArray(0);
-
-    // second, configure the light's VAO (VBO stays the same; the vertices are the same for the light object which is also a 3D cube)
-    unsigned int lightCubeVAO;
-    glGenVertexArrays(1, &lightCubeVAO);
-    glBindVertexArray(lightCubeVAO);
-
-    // we only need to bind to the VBO (to link it with glVertexAttribPointer), no need to fill it; the VBO's data already contains all we need (it's already bound, but we do it again for educational purposes)
-    glBindBuffer(GL_ARRAY_BUFFER, VBO);
-
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void *) 0);
-    glEnableVertexAttribArray(0);
-    glm::vec3 lightPos = glm::vec3(1.2f, 1.0f, 2.0f);
 
     while (!glfwWindowShouldClose(window)) {
         float currentFrame = glfwGetTime();
@@ -205,11 +111,6 @@ void render_loop(GLFWwindow *window) {
     //glDeleteBuffers(1, &triangle.EBO);
 }
 
-void framebuffer_size_callback(GLFWwindow *window, int width, int height) {
-    std::cout << "Framebuffer size: " << width << " x " << height << std::endl;
-    glViewport(0, 0, width, height);
-}
-
 void initOpenGl() {
     if (!glfwInit()) {
         std::cerr << "Failed to initialize GLFW\n";
@@ -246,29 +147,90 @@ void initOpenGl() {
     glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
     std::cout << "OpenGL version: " << glGetString(GL_VERSION) << "\n";
     glEnable(GL_DEPTH_TEST);
+    glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
+    glfwSetCursorPosCallback(window, mouse_callback);
+    glfwSetScrollCallback(window, scroll_callback);
     render_loop(window);
     glfwDestroyWindow(window);
     glfwTerminate();
 }
 
-void printVec4(const glm::vec4 &v) {
-    std::cout << v.x << " " << v.y << " " << v.z << " " << v.w << std::endl;
-}
-
-void printMatrix(const glm::mat4 &m) {
-    std::cout << "Print matrix:" << std::endl;
-    for (int row = 0; row < 4; ++row) {
-        std::cout << "[ ";
-        for (int col = 0; col < 4; ++col) {
-            std::cout << m[row][col] << " ";
-        }
-        std::cout << "]" << std::endl;
+void processInput(GLFWwindow *window) {
+    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) {
+        glfwSetWindowShouldClose(window, GLFW_TRUE);
     }
-    std::cout << std::endl;
+
+    const float cameraSensitivity = 0.05f;
+    if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS) {
+        camera.ProcessKeyboard(FORWARD, deltaTime);
+    }
+    if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS) {
+        camera.ProcessKeyboard(BACKWARD, deltaTime);
+    }
+
+    if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS) {
+        camera.ProcessKeyboard(LEFT, deltaTime);
+    }
+    if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS) {
+        camera.ProcessKeyboard(RIGHT, deltaTime);
+    }
+    if (glfwGetKey(window, GLFW_KEY_UP) == GLFW_PRESS) {
+        mixValue += 0.01f;
+        if (mixValue > 1.0f) {
+            mixValue = 1.0f;
+        }
+    }
+
+    if (glfwGetKey(window, GLFW_KEY_DOWN) == GLFW_PRESS) {
+        mixValue -= 0.01f;
+        if (mixValue < 0.0f) {
+            mixValue = 0.0f;
+        }
+    }
 }
 
+void mouse_callback(GLFWwindow *window, double xpos, double ypos) {
+    float xoffset = xpos - lastX;
+    float yoffset = lastY - ypos;
+    lastX = xpos;
+    lastY = ypos;
+    camera.ProcessMouseMovement(xoffset, yoffset);
+}
 
-// TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+void scroll_callback(GLFWwindow *window, double xoffset, double yoffset) {
+    camera.ProcessMouseScroll(yoffset);
+}
+
+unsigned int loadTexture(char const *path, bool invert = false) {
+    unsigned int texture;
+    glGenTextures(1, &texture);
+    glBindTexture(GL_TEXTURE_2D, texture);
+    // set the texture wrapping/filtering options (on the currently bound texture object)
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT); // horizontal
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT); // vertical
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR); // we cannot mip map on mag
+    // load and generate the texture
+    int width, height, nrChannels;
+    if (invert == false) {
+        stbi_set_flip_vertically_on_load(true);
+    }
+    unsigned char *data = stbi_load(path, &width, &height, &nrChannels, 0);
+    if (data) {
+        GLenum format = (nrChannels == 4) ? GL_RGBA : GL_RGB;
+        glTexImage2D(GL_TEXTURE_2D, 0, format, width, height, 0, format, GL_UNSIGNED_BYTE, data);
+        glGenerateMipmap(GL_TEXTURE_2D);
+    } else {
+        std::cout << "Failed to load texture" << std::endl;
+    }
+    stbi_image_free(data);
+    return texture;
+}
+
+void framebuffer_size_callback(GLFWwindow *window, int width, int height) {
+    std::cout << "Framebuffer size: " << width << " x " << height << std::endl;
+    glViewport(0, 0, width, height);
+} // TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 int main() {
     initOpenGl();
 
