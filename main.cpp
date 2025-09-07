e0b6ab39afd01fb2e47c95bffd08c8e5f74c7dcd
panagiotiskaragiannis <panoskarajohb@hotmail.com>
2025-09-07 11:03:51 +0100

    Add rendering for lamp object in main shader loop

--- main.cpp
+++ main.cpp
@@ -140,6 +140,18 @@ void render_loop(GLFWwindow *window) {
       glDrawArrays(GL_TRIANGLES, 0, 36);
     }
 
+    // also draw the lamp object
+    lightCubeShader.use();
+    lightCubeShader.setMat4("projection", projection);
+    lightCubeShader.setMat4("view", view);
+    model = glm::mat4(1.0f);
+    model = glm::translate(model, lightPos);
+    model = glm::scale(model, glm::vec3(0.2f)); // a smaller cube
+    lightCubeShader.setMat4("model", model);
+
+    glBindVertexArray(lightCubeVAO);
+    glDrawArrays(GL_TRIANGLES, 0, 36);
+
     glfwSwapBuffers(window);
     glfwPollEvents();
   }
